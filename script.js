efd88177891ab056424525032aefd70d9d48e6f3
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-11-26 21:00:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 'use strict';
 
+const date = new Date();
+document.querySelector('#footerDate').innerHTML = date.getFullYear();
 
+const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+            entry.target.src = entry.target.dataset.src;
+            observer.unobserve(entry.target);
+          }
+        });
+  },
+  {
+    rootMargin: "50px 0px 0px"
+  }
+);
+
+document.querySelectorAll('img').forEach((image) => {
+  imageObserver.observe(image);
+});
